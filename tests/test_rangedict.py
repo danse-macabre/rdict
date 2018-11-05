78f4a74e82b725063793e0c580245bcaa8355da4
Maksim Malyutin <m.malyutin@embedi.com>
2018-11-05 14:53:48 +0300

    Fix test_rangedict.py

--- tests/test_rangedict.py
+++ tests/test_rangedict.py
@@ -3,57 +3,57 @@ import rdict
 
 
 def test_items():
-    rdict = rangedict.RangeDict()
-    rdict[0:10] = 'a'
-    rdict[5:15] = 'b'
-    nose.tools.assert_equal(len(rdict), 2)
-    nose.tools.assert_equal(rdict[0:5], ['a'])
-    nose.tools.assert_equal(rdict[5:15], ['b'])
+    rangedict = rdict.RangeDict()
+    rangedict[0:10] = 'a'
+    rangedict[5:15] = 'b'
+    nose.tools.assert_equal(len(rangedict), 2)
+    nose.tools.assert_equal(rangedict[0:5], ['a'])
+    nose.tools.assert_equal(rangedict[5:15], ['b'])
 
-    rdict = rangedict.RangeDict()
-    rdict[0:15] = 'a'
-    rdict[-5:10] = 'b'
-    nose.tools.assert_equal(len(rdict), 2)
-    nose.tools.assert_equal(rdict[-5:10], ['b'])
-    nose.tools.assert_equal(rdict[10:15], ['a'])
+    rangedict = rdict.RangeDict()
+    rangedict[0:15] = 'a'
+    rangedict[-5:10] = 'b'
+    nose.tools.assert_equal(len(rangedict), 2)
+    nose.tools.assert_equal(rangedict[-5:10], ['b'])
+    nose.tools.assert_equal(rangedict[10:15], ['a'])
 
-    rdict = rangedict.RangeDict()
-    rdict[0:10] = 'a'
-    rdict[-5:15] = 'b'
-    nose.tools.assert_equal(len(rdict), 1)
-    nose.tools.assert_equal(rdict[-5:15], ['b'])
+    rangedict = rdict.RangeDict()
+    rangedict[0:10] = 'a'
+    rangedict[-5:15] = 'b'
+    nose.tools.assert_equal(len(rangedict), 1)
+    nose.tools.assert_equal(rangedict[-5:15], ['b'])
 
-    rdict = rangedict.RangeDict()
-    rdict[-5:15] = 'a'
-    rdict[0:10] = 'b'
-    nose.tools.assert_equal(len(rdict), 3)
-    nose.tools.assert_equal(rdict[-5:0], ['a'])
-    nose.tools.assert_equal(rdict[0:10], ['b'])
-    nose.tools.assert_equal(rdict[10:15], ['a'])
+    rangedict = rdict.RangeDict()
+    rangedict[-5:15] = 'a'
+    rangedict[0:10] = 'b'
+    nose.tools.assert_equal(len(rangedict), 3)
+    nose.tools.assert_equal(rangedict[-5:0], ['a'])
+    nose.tools.assert_equal(rangedict[0:10], ['b'])
+    nose.tools.assert_equal(rangedict[10:15], ['a'])
 
-    rdict = rangedict.RangeDict()
-    rdict[0:5] = 'a'
-    rdict[10:15] = 'a'
-    rdict[5:10] = 'a'
-    nose.tools.assert_equal(len(rdict), 1)
-    nose.tools.assert_equal(rdict[0:15], ['a'])
+    rangedict = rdict.RangeDict()
+    rangedict[0:5] = 'a'
+    rangedict[10:15] = 'a'
+    rangedict[5:10] = 'a'
+    nose.tools.assert_equal(len(rangedict), 1)
+    nose.tools.assert_equal(rangedict[0:15], ['a'])
 
-    rdict = rangedict.RangeDict()
-    rdict[0:5] = 'b'
-    rdict[10:15] = 'a'
-    rdict[5:10] = 'a'
-    nose.tools.assert_equal(len(rdict), 2)
-    nose.tools.assert_equal(rdict[0:5], ['b'])
-    nose.tools.assert_equal(rdict[5:15], ['a'])
+    rangedict = rdict.RangeDict()
+    rangedict[0:5] = 'b'
+    rangedict[10:15] = 'a'
+    rangedict[5:10] = 'a'
+    nose.tools.assert_equal(len(rangedict), 2)
+    nose.tools.assert_equal(rangedict[0:5], ['b'])
+    nose.tools.assert_equal(rangedict[5:15], ['a'])
 
-    rdict = rangedict.RangeDict()
-    rdict[0:5] = 'b'
-    rdict[5:10] = 'a'
-    rdict[3:8] = 'c'
-    nose.tools.assert_equal(len(rdict), 3)
-    nose.tools.assert_equal(rdict[0:3], ['b'])
-    nose.tools.assert_equal(rdict[3:8], ['c'])
-    nose.tools.assert_equal(rdict[8:10], ['a'])
+    rangedict = rdict.RangeDict()
+    rangedict[0:5] = 'b'
+    rangedict[5:10] = 'a'
+    rangedict[3:8] = 'c'
+    nose.tools.assert_equal(len(rangedict), 3)
+    nose.tools.assert_equal(rangedict[0:3], ['b'])
+    nose.tools.assert_equal(rangedict[3:8], ['c'])
+    nose.tools.assert_equal(rangedict[8:10], ['a'])
 
 
 if __name__ == "__main__":
